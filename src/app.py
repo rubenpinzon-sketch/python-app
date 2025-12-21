ebacf0ea188fdb33593bf8f3d5913df6c29996e6
Ruben.Pinzon <rpinzon@ext.laligatech.com>
2025-12-20 19:26:31 -0500

    fix of fix

--- src/app.py
+++ src/app.py
@@ -11,8 +11,8 @@ app = Flask(__name__)
 def details():
     return jsonify({
         'time': datetime.datetime.now().strftime("%I:%M:%S%p on %B %d, %Y"),
-        'hostname': socket.gethostname()
-        'message': 'You are doing great, human! :) '
+        'hostname': socket.gethostname(),
+        'message': 'You are doing great, human!'
     })
 
 @app.route('/api/v1/healthz')
