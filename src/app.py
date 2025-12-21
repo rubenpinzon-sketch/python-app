934815e7d1cfaa3ebb34932dba6206376ddc48a5
Ruben.Pinzon <rpinzon@ext.laligatech.com>
2025-12-21 18:38:32 -0500

    install python3

--- src/app.py
+++ src/app.py
@@ -12,7 +12,7 @@ def info():
     return jsonify({
         'time': datetime.datetime.now().strftime("%I:%M:%S%p on %B %d, %Y"),
         'hostname': socket.gethostname(),
-        'message': 'You are doing great, <3 human!!',
+        'message': 'You are doing great, <3 human!',
         'deployed_on': 'kubernetes'
     })
 
