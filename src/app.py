00a6f2a04ef28a300dd488b72b69d760b4434119
Dario Pinzon <rp@rubenchos.net>
2025-12-29 09:35:04 -0500

    update app.py file

--- src/app.py
+++ src/app.py
@@ -13,7 +13,7 @@ def info():
         'time': datetime.datetime.now().strftime("%I:%M:%S%p on %B %d, %Y"),
         'hostname': socket.gethostname(),
         'message': 'You are doing great, <3 human!',
-        'deployed_on': 'kubernetes'
+        'deployed_on': 'kubernetes!'
     })
 
 @app.route('/api/v1/healthz')
